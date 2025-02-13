2d724ca84238882eeba4bcf7d1aaa16a9d310a50
jli2786 <jli2786@wisc.edu>
2025-02-12 23:57:57 -0600

    1

--- models/var.py
+++ models/var.py
@@ -610,7 +610,7 @@ class SDVAR(nn.Module):
         for blk in self.target_model.blocks:
             blk.attn.kv_caching(False)   
                     
-        return target_f_hat   # de-normalize, from [-1, 1] to [0, 1]
+        return self.vae_proxy[0].fhat_to_img(f_hat).add_(1).mul_(0.5)   # de-normalize, from [-1, 1] to [0, 1]
 
 
 
