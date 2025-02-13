9586ea07be71bd7bbd753dd09d46924ee8046bca
jli2786 <jli2786@wisc.edu>
2025-02-12 22:18:58 -0600

    1

--- models/var.py
+++ models/var.py
@@ -397,6 +397,10 @@ class SDVAR(nn.Module):
         #     torch.cat((label_B, torch.full_like(label_B, fill_value=self.draft_model.num_classes)), dim=0)
         # )   # shape: (2B, C)
         sos = cond_BD = self.draft_model.class_emb(torch.cat((label_B, torch.full_like(label_B, fill_value=self.draft_model.num_classes)), dim=0))
+        print("draft cond_BD.shape:", cond_BD.shape)
+        print("draft num_classes:", self.draft_model.num_classes)
+        print("draft lable B.shape:",label_B.shape)
+        # print("draft ")
         # 原来是sos = cond_BD但是我不知道是为了什么
         lvl_pos = self.draft_model.lvl_embed(self.draft_model.lvl_1L) + self.draft_model.pos_1LC
         next_token_map = (
@@ -496,8 +500,9 @@ class SDVAR(nn.Module):
         
 
         sos = cond_BD = self.target_model.class_emb(torch.cat((label_B, torch.full_like(label_B, fill_value=self.target_model.num_classes)), dim=0))
-        print("draft cond_BD.shape:", cond_BD.shape)
-        print("draft cond_BD.shape:", cond_BD.shape)
+        print("cond_BD.shape:", cond_BD.shape)
+        print("num_classes:", self.draft_model.num_classes)
+        print("lable B.shape:",label_B.shape)
         lvl_pos = self.target_model.lvl_embed(self.target_model.lvl_1L) + self.target_model.pos_1LC
         # 这里存在疑惑，为什么我们需要生成一个first_token_map呢？难道说之前token_map不包括在里边吗？但似乎我们每次预测和保存的都是next_token_map而不是当前层的，这可能是其中的一个原因。
         first_token_map = sos.unsqueeze(1).expand(2 * B, self.target_model.first_l, -1) \
