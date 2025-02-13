6317866236b67932e9903a53648a0805464818ef
jli2786 <jli2786@wisc.edu>
2025-02-12 23:45:44 -0600

    1

--- models/var.py
+++ models/var.py
@@ -604,7 +604,7 @@ class SDVAR(nn.Module):
                 target_next_token_map = target_next_token_map.view(B, self.target_model.Cvae, -1).transpose(1, 2)
                 token_hub = torch.cat([token_hub,target_next_token_map],dim=1)
                 target_next_token_map = self.target_model.word_embed(target_next_token_map) + lvl_pos[:, target_cur_L:target_cur_L + self.patch_nums[si+1] ** 2]
-                target_ext_token_map = target_next_token_map.repeat(2, 1, 1)   # double the batch sizes due to CFG
+                target_next_token_map = target_next_token_map.repeat(2, 1, 1)   # double the batch sizes due to CFG
             
         # target模型生成完成
         for blk in self.target_model.blocks:
